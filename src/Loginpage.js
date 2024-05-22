b475d9449a23907df6e3d632959294c495de453b
1155194577 <1155194577@link.cuhk.edu.hk>
2024-05-22 08:45:45 +0800

    added login entries :Username and password Textfield , as well as login button

--- src/Loginpage.js
+++ src/Loginpage.js
@@ -1,20 +1,41 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useMutation } from 'react-query'
+import { Button, TextField } from '@mui/material'
+const post = (url,mutation,data) => {
+    console.log(url)
+    mutation.mutate(data)
+    console.log(mutation)
+    if (mutation.isSuccess) { 
+    console.log("Successfully posted data to server: ",mutation.variables)
+    console.log("Response from the server: ",mutation.data.data)}
+    if (mutation.isLoading) {
+        console.log("Isloading...")
+    }
+    if(mutation.isError) {
+        console.error("Error",mutation.error.message || "error occured")
+    }
+}
 
 
  function Loginpage() {
         const url = "https://backendapp-431f65cc5d32.herokuapp.com/post"   
         const mutation = useMutation((newPost)=>axios.post(url,newPost))
-        const post = () => {
-            console.log(url)
-           mutation.mutate({"test":"vdsdsds"})
-            console.log("Post successful : ",mutation.data.data["PostSuccessful"])
-        }
-       
-        
+        const [Username,SetUsername] = useState(null)
+        const [Password,SetPassword] = useState(null) 
 
-          return <div onClick={post}>test</div>
+        useEffect(
+            ()=>{
+                console.log(Username,Password)
+            },[Username,Password]
+        )
+
+          return( <div> 
+            <TextField variant="outlined" label="Username" type="email" onChange={(e)=>{SetUsername(e.target.value)}}/>
+            <TextField  variant="outlined" label="Password" type="email" onChange={(e)=>{SetPassword(e.target.value)}} />
+            <Button variant="contained" onClick={()=>{post(url,mutation,{Username:Password})}}>test</Button>
+        </div>
+    )
         }
     
 
